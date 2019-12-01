c128d67d952f9c5f5e234e5009891c4a7e421970
Cagatay Aydin <aydincagatay@yandex.com>
2019-12-01 19:49:48 +0300

    Tweak terser config.

--- webpack.config.js
+++ webpack.config.js
@@ -93,13 +93,46 @@ module.exports = {
     optimization: {
         minimizer: [
             new TerserPlugin({
-                cache: false,
-                parallel: true,
-                sourceMap: false, // Must be set to true if using source-maps in production
                 terserOptions: {
-                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-                    warnings: false,
+                    parse: {
+                        // We want terser to parse ecma 8 code. However, we don't want it
+                        // to apply any minification steps that turns valid ecma 5 code
+                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                        // sections only apply transformations that are ecma 5 safe
+                        // https://github.com/facebook/create-react-app/pull/4234
+                        ecma: 8,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        // Disabled because of an issue with Uglify breaking seemingly valid code:
+                        // https://github.com/facebook/create-react-app/issues/2376
+                        // Pending further investigation:
+                        // https://github.com/mishoo/UglifyJS2/issues/2011
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending further investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true, /* eslint-disable-line camelcase */
+                    },
                 },
+                // Use multi-process parallel running to improve the build speed
+                // Default number of concurrent runs: os.cpus().length - 1
+                parallel: true,
+                // Disable file caching
+                cache: false,
+                sourceMap: false, // Must be set to true if using source-maps in production
             }),
             new OptimizeCSSAssetsPlugin({}),
         ],
