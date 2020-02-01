bfdb99f0bec707349a9fee3af2905458f3a2ee75
Cagatay Aydin <aydincagatay@yandex.com>
2020-02-01 21:33:42 +0300

    Removed CSS in favor of SCSS. Using custom bootstrap build.

--- _src/base.js
+++ _src/base.js
@@ -2,7 +2,5 @@
 // Instead of Bootstrap JS which depends on jquery and popper
 // Use bootstrap.native JS, which is the vanilla implementation
 import 'bootstrap.native';
-// CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/main.css';
-import './css/icomoon.css';
+// SCSS
+import './scss/main.scss';
